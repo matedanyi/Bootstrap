2cf0a7f6109d2c291a6f2a3c4fcc7d2ad87e0f21
matedanyi <matedanyi@gmail.com>
2023-09-19 13:57:35 +0200

    Showcase of events

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+
+
+});
