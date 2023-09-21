bb0b966f72e9412b9c9d65b98e3993b1f14b0c7e
John Doe <zsoltee21@gmail.com>
2023-09-21 12:43:22 +0200

    Added js copntent

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector(".nav").addEventListener("mouseover", function (e) {
+  this.style.backgroundColor = randomColor();
+});
+
+console.log(randomInt(0, 255));
+
+console.log(randomColor(0, 255));
