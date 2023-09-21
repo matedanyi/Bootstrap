17e3c5eefebe085ad269bd994c927c44f91e382e
matedanyi <flashprince89@gmail.com>
2023-09-21 12:45:21 +0200

    change event

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-"use strict";
+'use strict';
 
 const randomInt = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min);
+    Math.floor(Math.random() * (max - min + 1) + min);
 
 const randomColor = () =>
-  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+    `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
 
-document.querySelector(".nav").addEventListener("mouseover", function (e) {
-  this.style.backgroundColor = randomColor();
+document.querySelector('.nav').addEventListener('onclick', function (e) {
+    this.style.backgroundColor = randomColor();
 });
 
 console.log(randomInt(0, 255));
